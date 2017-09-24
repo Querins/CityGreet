d7e9f0eed37c17acab47010e14c2c2e5f7e34dbb
Alex Querins <querins777@gmail.com>
2017-09-24 15:03:54 +0300

    Test for message correctness added

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,13 +1,80 @@
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
 
+import java.time.LocalTime;
 import java.time.ZoneId;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
 import static com.querins.Main.*;
 
 public class MainTest {
+
+    private Map<String, String> englishGreetings = new HashMap<>(4);
+    private Map<String, String> russianGreetings = new HashMap<>(4);
+    private String testCity = "Dnipro";
+
+    @Before
+    public void initGreetings() {
+
+        englishGreetings.put("morning", "Good morning");
+        englishGreetings.put("day", "Good day");
+        englishGreetings.put("evening", "Good evening");
+        englishGreetings.put("night", "Good night");
+
+        russianGreetings.put("morning", "Доброе утро");
+        russianGreetings.put("day", "Добрый день");
+        russianGreetings.put("evening", "Добрый вечер");
+        russianGreetings.put("night", "Доброй ночи");
+    }
+
+    @Test
+    @DisplayName("Test correctness of greeting messages")
+    public void testMessages() {
+
+        Locale locale = Locale.getDefault();
+
+        Set<String> timezones = ZoneId.getAvailableZoneIds();
+
+        Map targetGreetings = locale.getLanguage().equals(new Locale("ru").getLanguage()) ?
+                russianGreetings : englishGreetings;
+
+        for(String timezone: timezones) {
+
+            LocalTime time = LocalTime.now(ZoneId.of(timezone));
+            int hour = time.getHour();
+            String state;
+
+            if( hour >= 6 && hour < 9 ) {
+                state = "morning";
+            } else {
+                if ( hour >= 9 && hour < 19  ) {
+                    state = "day";
+                } else {
+                    if( hour >= 19 && hour <= 23 ) {
+                        state = "evening";
+                    } else {
+                        if( hour > 23 || hour < 6) {
+                            state = "night";
+                        } else {
+                            fail("Wrong value of hours: " + hour + " Should be in [0; 23]");
+                            return;
+                        }
+                    }
+                }
+            }
+
+            assertEquals("Testing message on timezone " + timezone,
+                    targetGreetings.get(state) + ", " + testCity + "!",
+                    getMessage(ZoneId.of(timezone), testCity));
+
+        }
+    }
 
     @Test(expected = IllegalArgumentException.class)
     @DisplayName("Wrong timezone test")
